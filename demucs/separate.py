6dc534353414bac8325aaeb6e42eb3b60f9e20b4
LzyPi8 <93503629+MaN0bhiR@users.noreply.github.com>
2024-04-09 14:30:41 +0530

    Update separate.py

--- demucs/separate.py
+++ demucs/separate.py
@@ -21,10 +21,10 @@ from .pretrained import add_model_flags, ModelLoadingError
 def get_parser():
     parser = argparse.ArgumentParser("demucs.separate",
                                      description="Separate the sources for the given tracks")
-    #parser.add_argument("tracks", nargs='*', type=str, default="AIR", help='Path to tracks')
+    parser.add_argument("tracks", nargs='*', type=str, default="AIR", help='Path to tracks')
     add_model_flags(parser)
     
-    parser.add_argument("--tracks", default="AIR", help='Path to folder of tracks')
+    #parser.add_argument("--tracks", default="AIR", help='Path to folder of tracks')
     parser.add_argument("--list-models", action="store_true", help="List available models "
                         "from current repo and exit")
     parser.add_argument("-v", "--verbose", action="store_true")
